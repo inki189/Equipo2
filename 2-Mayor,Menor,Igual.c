14ac131f2a8b75d5adbbfd888b687245bf3fa2ba
JoseBaezaP <josedelarosabass@gmail.com>
2018-02-01 18:19:55 -0600

    qa1

--- 2-Mayor,Menor,Igual.c
+++ 2-Mayor,Menor,Igual.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*Ejercicio 2
-autor:Carlos Alvarez
-Descripcion general:Algortimo para determinar si un numero es mayor,menor o igual con respecto a otro*/
+autor:Carlos Alvarez*/
+/*Descripcion general:Algortimo para determinar si un numero es mayor,menor o igual con respecto a otro*/
 int main ()
 {
 	/*Se definen las veriables*/
@@ -27,4 +27,13 @@ int main ()
 	/*salida:Se muestra en pantalla los valores obtenidos con su respectiva comparacion*/
 	return 0;
 }
+
+/*Q\A: el programa encuentra correctamente el numero
+mayor y si los numeros son iguales pero no genera
+una salida para indicar cual es el numero menor
+en el caso 1: los datos de entrada fueron 0 y 0 y
+los datos de salida fueron que eran iguales, en el 
+caso 2: los datos de entrada fueron -10 y 10 y los
+datos de salida fueron que 10 era mayor pero no hubo 
+un dato de salida para el menor. Q\A realizado por Carlos Eduardo Avila Criollo(Equipo 5 "dinamita"*/
 
