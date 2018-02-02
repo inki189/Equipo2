91e46dc6ab6beeb9ae2a10ed5f2f526af7c3a665
JoseBaezaP <josedelarosabass@gmail.com>
2018-02-01 18:20:25 -0600

    qa2

--- 8-Multiplos.c
+++ 8-Multiplos.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*Ejercicio 8
-Autor:Carlos Alvarez
-Descripcion general:Algortimo para determinar si un numero es multiplo o no de otro*/
+Autor:Carlos Alvarez*/
+/*Descripcion general:Algortimo para determinar si un numero es multiplo o no de otro*/
 int main(int argc, char *argv[]) {
 	/*Se definen las veriables*/
 	int numero,numerodos;
@@ -22,4 +22,10 @@ int main(int argc, char *argv[]) {
 	/*Salida:muestra el valor obtenido en base a la condicion*/
 	return 0;
 }
+
+/* Q\A: El programa funciona correctamente en el caso 1:
+los datos de entrada fueron 23 y 3 y los datos de 
+salida fueron que 3 no era multiplo de 23 y en el
+caso 2: los datos de entrada fueron 10 y -5 y los 
+datos de salida fueron que -5 si era multiplo de 10. Q\A realizado por Carlos Eduardo Avila Criollo(Equipo 5 "dinamita"*/
 
