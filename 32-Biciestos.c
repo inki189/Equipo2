8742ca001451526a11e200f65b87535a8137bb06
JoseBaezaP <josedelarosabass@gmail.com>
2018-02-01 18:23:00 -0600

    qa6

--- 32-Biciestos.c
+++ 32-Biciestos.c
@@ -32,3 +32,10 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
+/*Q\A: El programa funcion correctamente ya que te dice los años
+biciestos que hay entre los años que introduces sin tomar
+encuenta si el año inicial es biciesto, en el caso 1:
+los datos de entrada fueron 1900 y 2000 y el dato de salida
+fue 24 y en en el caso 2 los datos de entrada fueron 1900 y 
+2018 y el dato  de salida fue 29. Q\A realizado por Carlos Eduardo Avila Criollo(Equipo 5 "dinamita"*/
+
